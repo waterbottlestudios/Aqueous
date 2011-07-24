1f2928dab2359fb4230f48a75dba3367971cd231
Water Bottle Studios <waterbottlestudios@gmail.com>
2011-07-24 17:01:07 -0500

    Added second view and made basic interface

---
+++ Classes/MoreViewController.h
@@ -0,0 +1,16 @@
+//
+//  MoreClass.h
+//  Aqueous
+//
+//  Created by Dylan on 11-07-24.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MoreViewController : UIViewController {
+    
+}
+
+@end
